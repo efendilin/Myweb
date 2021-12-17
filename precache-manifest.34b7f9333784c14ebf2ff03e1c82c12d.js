fbf472649689139c2fba0faae16f951b22cd2b6a
efendilin <efendiloko@gmail.com>
2021-12-17 15:00:32 +0800

    First
    first

---
+++ precache-manifest.34b7f9333784c14ebf2ff03e1c82c12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8678a540c42d001ee34ec886e35bc37b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e534dbbeabc7c38e1e",
+    "url": "/static/js/2.546bbec0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.546bbec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c0b7fe9aabf57fce98",
+    "url": "/static/js/main.d2c963f0.chunk.js"
+  },
+  {
+    "revision": "86dbd99fb202bcff5a6e",
+    "url": "/static/js/runtime-main.18f81db4.js"
+  }
+]);
---
+++ precache-manifest.504ef35bd15e014819b4cd16dd1c9f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be2fade0710d967a9eb592f0f20a5e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e534dbbeabc7c38e1e",
+    "url": "/static/js/2.546bbec0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.546bbec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b56f0f9d110d1915927",
+    "url": "/static/js/main.44c96bc1.chunk.js"
+  },
+  {
+    "revision": "86dbd99fb202bcff5a6e",
+    "url": "/static/js/runtime-main.18f81db4.js"
+  }
+]);
